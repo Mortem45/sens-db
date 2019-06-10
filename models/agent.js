50a275b4313eecf3848cf47299540a5df8c06382
Mortem45 <ste45_007@hotmail.com>
2019-06-10 12:18:33 -0600

    add agent model

---
+++ models/agent.js
@@ -0,0 +1,37 @@
+'use strict'
+
+import Sequelize from 'sequelize'
+import setupDatabase from '../lib/db'
+
+module.exports = function setupAgentModel (config) {
+
+  const sequelize = setupDatabase(config)
+
+  return sequelize.define('agent', {
+    uuid: {
+      type: Sequelize.STRING,
+      allownull: false
+    },
+    username: {
+      type: Sequelize.STRING,
+      allownull: false
+    },
+    name: {
+      type: Sequelize.STRING,
+      allownull: false
+    },
+    hostname: {
+      type: Sequelize.STRING,
+      allownull: false
+    },
+    pid: {
+      type: Sequelize.INTEGER,
+      allownull: false
+    },
+    connected: {
+      type: Sequelize.BOOLEAN,
+      allownull: false,
+      defaultVAlue: false
+    }
+  })
+}
