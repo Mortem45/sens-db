4559320dc81a22f4f630d80c2860cfab3dbb7d7a
Mortem45 <ste45_007@hotmail.com>
2019-06-13 09:29:43 -0600

    Implements createOrUpdate

--- test/agent-tests.js
+++ test/agent-tests.js
@@ -13,10 +13,16 @@ let single = Object.assign({}, agentFixtures.single)
 let id = 1
 let AgentStub = null
 let db = null
+let uuid = 'yyy-yyy-yyy'
+let sandbox = null
 let config = {
   logging: function () {}
 }
-let sandbox = null
+let uuidArgs = {
+  where: {
+    uuid
+  }
+}
 
 test.beforeEach(async () => {
   sandbox = sinon.createSandbox()
@@ -24,7 +30,18 @@ test.beforeEach(async () => {
   AgentStub = {
     hasMany: sandbox.spy()
   }
+  // eslint-disable-next-line
+  // Model findOne Stub
+  AgentStub.findOne = sandbox.stub()
+  AgentStub.findOne.withArgs(uuidArgs).returns(Promise.resolve(agentFixtures.findByUuid(uuid)))
 
+  // eslint-disable-next-line
+  // Model update Stub
+  AgentStub.update = sandbox.stub()
+  AgentStub.update.withArgs(single, uuidArgs).returns(Promise.resolve(single))
+
+  // eslint-disable-next-line
+  // Model findById Stup
   AgentStub.findById = sandbox.stub()
   AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.findById(id)))
 
@@ -52,8 +69,20 @@ test.serial('Setup DB', t => {
 
 test.serial('Agent#findById', async t => {
   let agent = await db.Agent.findById(id)
+
   t.true(AgentStub.findById.called, 'findById should be called on model')
   t.true(AgentStub.findById.calledOnce, 'findById should be called once')
   t.true(AgentStub.findById.calledWith(id), 'findById should be called with specified id')
+
   t.deepEqual(agent, agentFixtures.findById(id), 'should be the same')
+})
+
+test.serial('Agent#createOrUpdate - exists', async t => {
+  let agent = await db.Agent.createOrUpdate(single)
+
+  t.true(AgentStub.findOne.called, 'findOne should be called on model')
+  t.true(AgentStub.findOne.calledTwice, 'findOne should be called twice')
+  t.true(AgentStub.update.calledOnce, 'update should be called once')
+
+  t.deepEqual(agent, single, 'Agent should be the same')
 })
